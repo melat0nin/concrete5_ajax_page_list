3d26a8cdb3bc1b7bd3bac52ff3573f41151516a6
Laurence Diver <laurence@39steps.co.uk>
2013-04-02 12:47:22 +0100

    Pagination and PageList filtering working

--- templates/ajax_paging.php
+++ templates/ajax_paging.php
@@ -1,26 +1,45 @@
 <?php
 defined('C5_EXECUTE') or die("Access Denied.");
 
-$bt = BlockType::getByHandle('page_list');
-$ajax_request_url = Loader::helper('concrete/urls')->getBlockTypeToolsURL($bt).'/ajax_request';
+// Vars
+$c_id = Page::getCurrentPage()->getCollectionID();
 
+// Helpers
+$th = Loader::helper('text');
+
+// Get block tools URL for ajax request script
+$bt = BlockType::getByHandle('page_list');
+$block_tools_dir = Loader::helper('concrete/urls')->getBlockTypeToolsURL($bt);
+$ajax_request_url = $block_tools_dir . '/ajax_request';
+
+/*
+ *  Add filters to query string according to block parameters set by user
+ */
+$ajax_request_url .= '?bID=' . $controller->bID;			    // # of results per page
+$ajax_request_url .= '&num=' . $controller->num;			    // # of results per page
+$ajax_request_url .= '&cParentID=' . $controller->cParentID;		    // Parent page ID
+$ajax_request_url .= '&cThis=' . $controller->cThis;			    // Filter beneath this page
+$ajax_request_url .= '&orderBy=' . $controller->orderBy;		    // Order results
+$ajax_request_url .= '&ctID=' . $controller->ctID;			    // Page type ID
+//$ajax_request_url .= '&rss=' . $controller->rss;			    // RSS
+$ajax_request_url .= '&displayAliases=' . $controller->displayAliases;	    // Display aliases
+$ajax_request_url .= '&displayFeaturedOnly=' . $controller->displayFeaturedOnly;	    // Display aliases
+$ajax_request_url .= '&cID=' . $controller->cID;					    // Current page's ID
 ?>
 
 <div class="ccm-page-list">
-
     <div id="ajax-pages"></div>
-
 </div>
 
 <script>
-$(document).ready(function(ev) {
-    $('#ajax-pages').text('Loading...').load(<?=$ajax_request_url?>);
+$(document).ready(function() {
+    $('#ajax-pages').html('<img src="<?=$this->getBlockURL()?>/loading.gif" alt="Loading pages..." />').load('<?=$ajax_request_url?>');
 
     $('#ajax-paginator a').live('click', function(ev) {
         ev.preventDefault();
         var link_href = $(this).attr('href');
 
-        $('#ajax-pages').text('Loading...').load(link_href);
+        $('#ajax-pages').html('<img src="<?=$this->getBlockURL()?>/loading.gif" alt="Loading pages..." />').load(link_href);
 
         return false;
     });
--- tools/ajax_request.php
+++ tools/ajax_request.php
@@ -1,50 +1,117 @@
 <?php 
 
 defined('C5_EXECUTE') or die("Access Denied.");
- 
 
-// Get PageList filters from query
-$parentCID = intval( $_GET['parent_page_id'] );	    // Filter by parent ID
-$collectionTypeHandle = $_GET['page_type'];	    // Filter by page type
-
-
-// Get Pagination filters from query
-$page = intval( $_GET['ccm_paging_p'] );	    // Requested page of results
-$page = empty($page) ? 1 : $page;		    // Paginator returns this page of results
+// Helpers
+$nh = Loader::helper('navigation');
 
 // Instantiate PageList object
 Loader::model('page_list');
+$db = Loader::db();
+$bID = $_GET['bID'];
+if ($bID) {
+    $q = "select num, cParentID, cThis, orderBy, ctID, displayAliases, rss, displayFeaturedOnly, includeAllDescendents from btPageList where bID = '$bID'";
+    $r = $db->query($q);
+    if ($r) {
+	$row = $r->fetchRow();
+    }
+    $row['cID'] = $_GET['cID'];
+} else {
+    $row = $_GET;
+}
+
 $pl = new PageList();
-$pl->sortByName();
+$pl->setNameSpace('b' . $bID);
 
-// Apply filters
-if (!empty($collectionTypeHandle)) {
-    $pl->filterByCollectionTypeHandle($collectionTypeHandle);
-}
-if (!empty($parentCID)) {
-    $pl->filterByParentID($parentCID);
+/*
+ *  Apply filters
+ */
+
+// Sort results
+switch($row['orderBy']) {
+    case 'display_asc':
+	$pl->sortByDisplayOrder();
+	break;
+    case 'display_desc':
+	$pl->sortByDisplayOrderDescending();
+	break;
+    case 'chrono_asc':
+	$pl->sortByPublicDate();
+	break;
+    case 'alpha_asc':
+	$pl->sortByName();
+	break;
+    case 'alpha_desc':
+	$pl->sortByNameDescending();
+	break;
+    default:
+	$pl->sortByPublicDateDescending();
+	break;
 }
 
-// Set up pagination
-$pl->setItemsPerPage(3);
-if ($pl->getSummary()->pages > 1) {
+// Filter by is_featured attribute
+if ($row['displayFeaturedOnly'] == 1) {	    
+    Loader::model('attribute/categories/collection');
+    $cak = CollectionAttributeKey::getByHandle('is_featured');
+    if (is_object($cak)) {
+	$pl->filterByIsFeatured(1);
+    }
+}
+
+// Display page aliases
+if (!$row['displayAliases']) {	    
+    $pl->filterByIsAlias(0);
+}
+$pl->filter('cvName', '', '!=');
+
+// Filter by page type ID
+if ($row['ctID']) {	
+    $pl->filterByCollectionTypeID($row['ctID']);
+}
+
+// Filter by exclude from page list
+$columns = $db->MetaColumns(CollectionAttributeKey::getIndexedSearchTable());	
+if (isset($columns['AK_EXCLUDE_PAGE_LIST'])) {
+    $pl->filter(false, '(ak_exclude_page_list = 0 or ak_exclude_page_list is null)');
+}
+
+// Set parent page
+if ( intval($row['cParentID']) != 0) {
+    $c = Page::getCurrentPage();
+    $cParentID = ($row['cThis']) ? $row['cID'] : $row['cParentID'];
+    if ($row['includeAllDescendents']) {
+	$pl->filterByPath(Page::getByID($cParentID)->getCollectionPath());
+    } else {
+	$pl->filterByParentID($cParentID);
+    }
+}
+
+/*
+ *  Set up pagination
+ */
+$num = (int) $row['num'];
+$pl->setItemsPerPage($num);
+$current_page_get_var = 'ccm_paging_p_b' . $bID;
+$current_page = intval( $_GET[$current_page_get_var] );		// Requested page of results
+$current_page = empty($current_page) ? 1 : $current_page;	// Paginator returns this page of results
+
+if ($pl->getSummary()->pages > 1) {	// Retrieve pagination links ready for display
     $paginator = $pl->getPagination();
     $paginator_links = $paginator->getPages();
 }
 
-// Retrieve pages
-$pages = $pl->getPage($page);
- 
-// Output pages
-$nh = Loader::helper('navigation');
+/*
+ * Retrieve and output pages and pagination
+ */
+$pages = $pl->getPage($current_page);
+
 foreach ($pages as $page) {
   echo '<article>';
   echo '<a href="' . $nh->getLinkToCollection($page) . '">' . htmlentities($page->getCollectionName()) . '</a>';  
   echo '</article>';
 }
 
-// Output pagination
-if ( !empty($paginator_links) ) {
+if ( !empty($paginator_links) ) {   // Output pagination
     echo '<div id="ajax-paginator" class="pagination">';
     echo $paginator_links;
     echo '</div>';
