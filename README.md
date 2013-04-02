ad15907d89d7f08309b98b5056029f63f5f74b3e
Laurence Diver <laurence@39steps.co.uk>
2013-04-02 16:52:03 +0100

    Refactored into concrete5 package

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ concrete5_ajax_page_list
 ========================
 
 An ajaxified page list block for concrete5
+
+concrete5_ajax_page_list
+========================
+
+An ajaxified page list block for concrete5
+
+To install
+----------
+
+1. Download the ZIP above
+2. Extract the directory and rename it to ajax_page_list
+3. Upload to your /packages directory
+4. Install through the concrete5 Dashboard
+5. Select the Ajax Page List custom template
+6. Done!
---
+++ controller.php
@@ -0,0 +1,22 @@
+<?php      
+defined('C5_EXECUTE') or die(_("Access Denied."));
+
+class AjaxPageListPackage extends Package {
+	
+	protected $pkgHandle = 'ajax_page_list';
+	protected $appVersionRequired = '5.5';
+	protected $pkgVersion = '1.0';
+	
+	public function getPackageName() {
+		return t('Ajax Page List'); 
+	}
+	
+	public function getPackageDescription() {
+		return t('Loads and paginates Page List entries using AJAX');
+	}
+
+	public function install() {
+		parent::install();
+	}
+
+}
